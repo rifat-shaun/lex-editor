ab07c51766b6797ef7bed30a1e8aaa99e82dd8cb
rifat-hasan <mdrifathasanshaun@gmail.com>
2025-04-19 13:10:51 +0600

    feat: tailwind css issue fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { LexicalEditor } from './lexical/LexicalEditor'
 function App() {
   return (
     <div className="App">
-      {/* <LexicalEditor /> */}
-      <div className="text-3xl font-bold">Hello World</div>
+      <LexicalEditor />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
     content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}', // include all JSX/TSX files in src
     ],
     theme: {
       extend: {},
     },
     plugins: [],
-  }
+  };
   
